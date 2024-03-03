6fc185ac4569c8f3ef171b95c56a702a5d24c19c
robynskyrme <robynskyrme@gmail.com>
2024-03-03 09:36:29 +0000

    Leetcode 1 (using a hashtable dictionary, down from O(n^2) to O(n)

--- 150/1_Two-Sum.py
+++ 150/1_Two-Sum.py
@@ -1,5 +1,3 @@
-
-
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -8,24 +6,14 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        sol_a = 0
-        sol_b = 0
+        differences = {}
 
-                            # Simply loop through the whole list twice, checking for correct sum
-        for b in range(len(nums)):
-            for a in range(len(nums)):
-
-                sum = nums[a] + nums[b]
-
-                            # If correct, store the results (that there is only one is given)
-                            # wihle checking that a number is not being added to itself
-                if sum == target and a != b:
-                    sol_a = a
-                    sol_b = b
-                    break
-
-        return [sol_a,sol_b]
-
+        for i in range(len(nums)):
+            difference = target - nums[i]
+            if difference in differences:
+                return [differences[difference], i]
+            else:
+                differences[nums[i]] = i
 
 if __name__ == "__main__":
     print(Solution.twoSum(Solution,[4,2,7,6,3],8))
