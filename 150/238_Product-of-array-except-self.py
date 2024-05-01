22a9ebb0c66562e49299a3cf477255301ab8803c
robynskyrme <robynskyrme@gmail.com>
2024-05-01 13:06:12 +0100

    Leetcode 238 (barely started)

---
+++ 150/238_Product-of-array-except-self.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+                            # copy intput array, zero it
+        output = []
+        filter = []
+
+
+
+        return output
+
+
+if __name__ == "__main__":
+    print(Solution.productExceptSelf(Solution,[1,2,3,4]))
